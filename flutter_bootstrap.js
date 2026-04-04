b74da12eb075bc0e3ea72184157ffd8dcb690ae3
Yejiaomin <Yejiaomin@users.noreply.github.com>
2026-04-04 03:44:49 +0000

    deploy: 4745fc76f6d7b668aa1eeb5d48d2005bdfee3e70

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1603176721" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "176744469" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
